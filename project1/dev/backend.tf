3e24e08dd6bfcc42f72b9ebe52e823fc6b6c79a8
rakesh-personal-1 <rakeshbharadwaj.adluri@gmail.com>
2024-04-30 15:01:28 +0530

    Backend addition

---
+++ project1/dev/backend.tf
@@ -0,0 +1,46 @@
+#Create an S3 bucket
+resource "aws_s3_bucket" "bucket" {
+    bucket = "auroradev-terraform-state-backend"
+    versioning {
+        enabled = true
+    }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+    tags = {
+        Name = "S3 Remote Terraform State Store"
+    }
+}
+
+#Create DynamoDB table
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "terraform_state"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
+
+#Map S3 and DynamoDB as the backend
+
+terraform {
+  backend "s3" {
+    bucket         = "angelo-terraform-state-backend"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform_state"
+  }
+}
